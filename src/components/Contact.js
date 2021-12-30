6665f20d284604c389dab8f0dba9258882322513
Tbtimber <delpierre.matthieu@gmail.com>
2021-12-30 20:58:54 +0100

    Added content to breathing page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,11 @@
+import InstaFeeds from "./InstagramFeed"
+
 function MaioraContact() {
     return (
         <div className="content-test">
-            CONTACT
+            <InstaFeeds token="271319565040108|a5a796a41ff0e71f41581acbdce19f18" limit={12}/>
         </div>
-    )
+    ) 
 }
 
 export default MaioraContact
