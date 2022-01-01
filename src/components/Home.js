c646a00f6857714c4f7cd1072eae12b99eff281b
Tbtimber <delpierre.matthieu@gmail.com>
2022-01-01 19:43:46 +0100

    Home page header + Who am i articles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
+import HomePageHeaderArticle from "./articles/HomePageHeader"
+import HomeWho from "./articles/HomeWho"
+
 function MaioraHome() {
     return (
         <div className="maiora-home-content-div">
-            
+            <HomePageHeaderArticle />
+            <HomeWho />
         </div>
     )
 }
