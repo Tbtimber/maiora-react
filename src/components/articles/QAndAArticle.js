8c134fa9c0078996079e7ef7521064b85cbfef05
Tbtimber <delpierre.matthieu@gmail.com>
2022-01-01 13:31:30 +0100

    Breahting continued

--- src/components/articles/QAndAArticle.js
+++ src/components/articles/QAndAArticle.js
@@ -2,11 +2,11 @@ import '../../styles/QAndAArticle.css';
 
 function QAndAArticle({question, answer, buttonLabel, displayBt}) {
     return (
-        <article class="maiora-article-qa">
-            <h1 class="maiora-qa-question">{ question }</h1>
-            <p class="maiora-qa-answer">{ answer }</p>
+        <article className="maiora-article-qa">
+            <h1 className="maiora-qa-question">{ question }</h1>
+            <p className="maiora-qa-answer">{ answer }</p>
             { displayBt && 
-                <button class="maiora-qa-button">{ buttonLabel }</button>
+                <button className="maiora-qa-button">{ buttonLabel }</button>
             }   
         </article>
     )
